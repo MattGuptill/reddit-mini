56606b0ec2d1742dc1d11bbe4401dff251fe64e9
Matthew Guptill <mattguptill@gmail.com>
2023-05-29 17:00:26 -0500

    components created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import './App.css';
+import Article from './Components/Article/Article'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div className="nav-bar">
+          <input type="text"></input>
+          <h1>REDDIT MINI</h1>
+          <button>press me</button>
+        </div>
       </header>
+      <main className="main-container">
+        <Article />
+      </main>
     </div>
   );
 }
