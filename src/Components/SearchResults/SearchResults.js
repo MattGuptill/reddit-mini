ce441dda22496202d95b5e5bb4a69aba6d41ba55
Matthew Guptill <mattguptill@gmail.com>
2023-05-30 09:58:08 -0500

    searchResults set up

---
+++ src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {useSearchParams} from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import {useEffect} from 'react';
+
+
+import fetchResults from '../../Features/searchSlice';
+
+export default function SearchResults() {
+    let results = useSelector(state => state.search.results);
+    let isLoading = useSelector(state => state.search.isLoading);
+    let error = useSelector(state => state.search.hasError);
+    let [searchParams] = useSearchParams();
+    let term = searchparams.get('q');
+    let dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchResults(term))
+    }, [dispatch, error, term]);
+
+    return (
+        <div>
+        hello World
+        </div>
+    )
+
+}
